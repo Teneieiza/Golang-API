1c0e972dab70a1a96b5a04c0fed2451282d00eca
tenmonster <tenmonster@hotmail.com>
2025-03-18 18:04:14 +0700

    Setup Project

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Teneieiza/Golang-API/webservice"
+	// "github.com/Teneieiza/Golang-API/marshal"
+)
+
+func main() {
+	fmt.Println("Hello, World! with GoLang eieiza")
+
+	// marshal.Marshal()
+	// marshal.UnMarshal()
+	webservice.WorkRequest()
+}
