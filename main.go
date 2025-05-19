0be9f169c69fd0edb212836b7313c4a2298cce5a
tenmonster <tenmonster@hotmail.com>
2025-05-19 16:17:53 +0700

    Add path of project

--- main.go
+++ main.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
-	"github.com/Teneieiza/Golang-API/webservice"
-	// "github.com/Teneieiza/Golang-API/marshal"
+	"github.com/Teneieiza/Golang-API/middleware"
+	"github.com/Teneieiza/Golang-API/config"
+	"github.com/Teneieiza/Golang-API/routes"
+	// "github.com/Teneieiza/Golang-API/00-marshal"
+	// webservice "github.com/Teneieiza/Golang-API/01-webservice"
+	// urlpath "github.com/Teneieiza/Golang-API/02-urlpath"
+	// middleware "github.com/Teneieiza/Golang-API/03-middleware"
+	// corsorigin "github.com/Teneieiza/Golang-API/04-corsorigin"
+	// connectDB "github.com/Teneieiza/Golang-API/05-connectDB"
+	// gowithmysql "github.com/Teneieiza/Golang-API/06-gowithmysql"
+	// apiwithdb "github.com/Teneieiza/Golang-API/07-APIwithDB"
 )
 
 func main() {
@@ -12,5 +23,17 @@ func main() {
 
 	// marshal.Marshal()
 	// marshal.UnMarshal()
-	webservice.WorkRequest()
+	// webservice.WorkRequest()
+	// urlpath.UrlPath()
+	// middleware.Middleware()
+	// corsorigin.Corsorigin()
+	// connectDB.ConnectDB()
+	// gowithmysql.Gowithmysql()
+	// apiwithdb.ApiWithDB()
+	config.SetupDB()
+	routes.SetupRoutes()
+
+	log.Println("Server listening on http://localhost:5000")
+	http.ListenAndServe(":5000", middleware.CorsMiddleware(http.DefaultServeMux))
+
 }
